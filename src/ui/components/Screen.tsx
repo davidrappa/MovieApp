d724e4029742f37559b653397f6be4dcedd9bfc7
David Rappa <davidrappa@outlook.com>
2025-07-23 16:16:43 -0300

    feat: add common components like text, box and screen

---
+++ src/ui/components/Screen.tsx
@@ -0,0 +1,25 @@
+import {PropsWithChildren} from 'react';
+import {KeyboardAvoidingView, Platform, ScrollView, View} from 'react-native';
+import {Box, BoxProps} from './Box';
+
+export function Screen({
+  children,
+  scrollable = false,
+  ...boxProps
+}: PropsWithChildren & BoxProps & {scrollable?: boolean}) {
+  const Container = scrollable ? ScrollView : View;
+
+  return (
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <Box
+        flex={1}
+        backgroundColor="gray100"
+        paddingHorizontal="s20"
+        {...boxProps}>
+        <Container showsVerticalScrollIndicator={false}>{children}</Container>
+      </Box>
+    </KeyboardAvoidingView>
+  );
+}
