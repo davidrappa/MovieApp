0ce40db068d473122d91a48499d705c97b54fa0a
David Rappa <davidrappa@outlook.com>
2025-07-24 16:49:11 -0300

    feat: add BackButton component for navigating back in the app

---
+++ src/ui/components/BackButton.tsx
@@ -0,0 +1,29 @@
+import { router } from "expo-router";
+import { TouchableOpacityBox } from "./Box";
+import { Icon } from "./Icon";
+import { Text } from "./Text";
+
+export function BackButton() {
+  function onPress() {
+    router.back();
+  }
+
+  return (
+    <TouchableOpacityBox
+      onPress={onPress}
+      backgroundColor="gray300"
+      alignSelf="baseline"
+      borderRadius="s8"
+      flexDirection="row"
+      gap="s8"
+      alignItems="center"
+      paddingHorizontal="s10"
+      paddingVertical="s6"
+    >
+      <Icon name="arrowLeft" size={20} color="gray600" />
+      <Text variant="textMD" color="gray600">
+        Voltar
+      </Text>
+    </TouchableOpacityBox>
+  );
+}
