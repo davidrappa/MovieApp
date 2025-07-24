f6a345c92a3bb67996f6b18e719388353bf555ab
David Rappa <davidrappa@outlook.com>
2025-07-24 10:51:26 -0300

    feat: migrate app configuration from app.json to app.config.js

---
+++ app.config.js
@@ -0,0 +1,47 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "MovieApp",
+    slug: "MovieApp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "movieapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      tmdbApiKey: process.env.TMDB_API_KEY,
+    },
+  },
+};
